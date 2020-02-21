a3429bf12b5bb76f5d53fa6a22b25d97c34f91ac
unknown <1173726398@qq.com>
2020-02-21 22:34:56 +0800

    45th day

--- JavaScript2/tool.js
+++ JavaScript2/tool.js
@@ -163,3 +163,49 @@ function limitDrag(node){
         document.onmousemove = null;
     }
 }
+
+//所有物体运动
+function startMove(node, cssObj, complete){
+    clearInterval(node.timer);
+    node.timer = setInterval(function(){
+        var isEnd = true;  //假设所有动画都到达目的值了，
+        for(var attr in cssObj){
+            var iTarget = cssObj[attr];
+            //计算速度
+            var iCur = null;
+            if(attr == 'opacity'){
+                iCur = parseInt(parseFloat(getStyle(node, 'opacity')) * 100);
+            }else{
+                iCur = parseInt(getStyle(node, attr));
+            }
+            var speed = (iTarget - iCur) / 8;
+            speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+
+            
+                if(attr == 'opacity'){
+                    iCur += speed;
+                    node.style.opacity = iCur / 100;
+                    node.style.filter = `alpha(opacity=${iCur})`;
+                }else{
+                    node.style[attr] = iCur + speed + 'px';
+                }
+
+                if(iCur != iTarget){
+                    isEnd = false;
+                }
+            
+        }
+
+        if(isEnd){
+            clearInterval(node.timer);
+            if(complete){
+                complete.call(node);
+            }
+        }
+    }, 30);
+}
+
+//跨浏览器兼容获取当前css样式
+function getStyle(node, cssStyle){
+return node.currentStyle ? node.currentStyle[cssStyle] : getComputedStyle(node)[cssStyle];
+}
